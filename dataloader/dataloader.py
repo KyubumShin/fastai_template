806d7b62da22169b03d0fe58643b40b3eb6536bd
KyubumShin <kbum0617@naver.com>
2022-02-23 12:34:23 +0900

    update fastai baseline

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -1,10 +1,14 @@
 from fastai.vision.all import *
 
 
-def get_data(config):
-    dls = ImageDataLoaders.from_folder(config.data.train_path,
-                                       test=config.data.test_path,
+def get_data(config, transpose):
+    dls = ImageDataLoaders.from_folder(config.data.data_path,
+                                       vaild_pct=1/config.data.num_fold,
+                                       seed=config.seed,
+                                       item_tfms=transpose,
+                                       bs=config.data.batch_size,
                                        )
+    return dls
 
 
 
